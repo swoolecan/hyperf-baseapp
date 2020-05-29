8ff309b2ee720109f56a9a0725e908010cd4af42
wangcanliang <wangcanliang@weinaya.com>
2020-05-29 19:31:22 +0800

    develop baseapp

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# hyperf-baseapp
-The base app of hyperf
+# Swoole框架认证功能封装，
+
+使用示例代码：<https://github.com/candocker/passport>
+
+## 安装
+
+ ```
+  composer require wangcan/swoole-auth
+ ```
+发布配置
+```
+ php bin/hyperf.php vendor:publish wangcan/swoole-auth
+```
+修改配置文件config/autoload/jwt.php
+
+数据库迁移
+
+```
+php bin/hyperf.php migrate
+```
+
+## CHANGELOG
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ src/Models/Model.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types = 1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Swoolecan\Baseapp\Model;
+
+use Hyperf\DbConnection\Model\Model as BaseModel;
+use Hyperf\ModelCache\Cacheable;
+use Hyperf\ModelCache\CacheableInterface;
+
+abstract class Model extends BaseModel implements CacheableInterface
+{
+    use Cacheable;
+
+    public static $status = [
+        0 => '禁用',
+        1 => '正常'
+    ];
+
+    const STATUS_DELETED = -1; //status 为-1表示删除
+    const STATUS_DISABLE = 0; //status 为0表示未启用
+    const STATUS_ENABLE = 1; //status 为1表示正常
+    
+    function getFormatState($key = 0, $enum = array(), $default = '')
+    {
+        return array_key_exists($key, $enum) ? $enum[$key] : $default;
+    }
+}
