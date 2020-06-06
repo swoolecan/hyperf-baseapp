77bef77b2f5dc0c01feaf6f1f04ccb3bb97034f2
wangcanliang <wangcanliang@weinaya.com>
2020-06-06 10:36:29 +0800

    develop baseapp

---
+++ src/Contracts/Repositories/RepositoryInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Swoolecan\Baseapp\Contracts\Repositories;
+
+interface RepositoryInterfase
+{
+}
---
+++ src/Resources/DemoCollection.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+
+namespace Swoolecan\Baseapp\Resources;
+
+//use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class DemoCollection //extends ResourceCollection
+{
+    public function toArray($request)
+    {
+        return $this->collection->map(function ($item) {
+            return [
+                'nickname' => $item->name,
+                'email' => $item->email,
+            ];
+        })->all();
+    }
+}
