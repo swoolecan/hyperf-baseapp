877929d5dd8e99cf353d3c873ffa30ca7fccb1ba
wangcanliang <wangcanliang@weinaya.com>
2020-08-29 08:49:57 +0800

    develop baseapp

--- src/Middleware/PermissionMiddleware.php
+++ src/Middleware/PermissionMiddleware.php
@@ -10,12 +10,14 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Swoolecan\Baseapp\Helper\SysOperation;
+use Swoolecan\Baseapp\JsonRpcClient\PassportBaseService;
 
-class InitResourceMiddleware implements MiddlewareInterface
+class PermissionMiddleware implements MiddlewareInterface
 {
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        Context::set('resources', SysOperation::initResourceDatas());
+        $passportBase = make(PassportBaseService::class);
+        $token = $request->token;
         return $handler->handle($request);
     }
 
