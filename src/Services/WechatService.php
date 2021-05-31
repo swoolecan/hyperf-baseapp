34ad22185111fa32bcf18854792ed2c48a984790
wangcanliang <wangcanliang@weinaya.com>
2021-05-31 11:33:29 +0800

    develp hyperf baseapp

---
+++ src/Services/WechatService.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Framework\Baseapp\Services;
+
+use Swoolecan\Foundation\Services\TraitWechatService;
+
+class WechatService extends AbstractService
+{
+    use TraitWechatService;
+}
